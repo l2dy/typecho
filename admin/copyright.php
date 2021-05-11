bc2c38d503ad2a816148a513a9f4a92318f47cd5
Zero King <l2dy@icloud.com>
2021-05-11 16:50:19 +0000

    https

--- admin/copyright.php
+++ admin/copyright.php
@@ -2,12 +2,12 @@
 <div class="typecho-foot" role="contentinfo">
     <div class="copyright">
         <i class="i-logo-s">Typecho</i>
-        <p><?php _e('由 <a href="http://typecho.org" rel="noreferrer nofollow">%s</a> 强力驱动, 版本 %s (%s)', $options->software, $prefixVersion, $suffixVersion); ?></p>
+        <p><?php _e('由 <a href="https://typecho.org" rel="noreferrer nofollow">%s</a> 强力驱动, 版本 %s (%s)', $options->software, $prefixVersion, $suffixVersion); ?></p>
     </div>
     <nav class="resource">
-        <a href="http://docs.typecho.org" rel="noreferrer nofollow"><?php _e('帮助文档'); ?></a> &bull;
-        <a href="http://forum.typecho.org" rel="noreferrer nofollow"><?php _e('支持论坛'); ?></a> &bull;
+        <a href="https://docs.typecho.org" rel="noreferrer nofollow"><?php _e('帮助文档'); ?></a> &bull;
+        <a href="https://forum.typecho.org" rel="noreferrer nofollow"><?php _e('支持论坛'); ?></a> &bull;
         <a href="https://github.com/typecho/typecho/issues" rel="noreferrer nofollow"><?php _e('报告错误'); ?></a> &bull;
-        <a href="http://extends.typecho.org" rel="noreferrer nofollow"><?php _e('资源下载'); ?></a>
+        <a href="https://extends.typecho.org" rel="noreferrer nofollow"><?php _e('资源下载'); ?></a>
     </nav>
 </div>
